6ea5d08d45885a39ffab152771419084e6511a86
alessiam1909 <ale.menon.vr@gmail.com>
2022-11-30 20:37:23 +0100

    conclusione secondo esercizio con codice js

--- Gioco dei dadi/Js/script.js
+++ Gioco dei dadi/Js/script.js
@@ -5,20 +5,25 @@ console.log(numUtente);
 
 
 //  Se il numero è maggiore a 6 non prosegue con il calcolo
-if (numUtente > 6){
-    console.log("Il numero inserito è maggiore di 6");
-    alert("Il numero inserito è maggiore di 6, riprova");
+if (numUtente > 6 || numUtente < 1){
+    console.log("Il numero inserito è maggiore di 6 o minore di 1");
+    alert("Il numero inserito è maggiore di 6 o minore di 1, riprova");
 }
 //  Genero un numero randomico da 1 a 6 per il computer
-let numComputer = Math.round(Math.random() * 6);
+let numComputer = Math.round(Math.random() * 6)+ 1;
 console.log(numComputer);
 
-// Se il numero utente è maggiore del numero pc l'utente ha vinto, altrimenti vince il pc
-if(numUtente > numComputer ){
-    console.log("Hai vinto!");
-    alert("Hai vinto!");
-} else {
-    console.log("Hai perso!");
-    alert("Hai perso!");
-};
+// Se il numero è minore di 6 ma maggiore di 0 prosegue con il calcolo
+if(numUtente < 7 && numUtente > 0){
+
+
+    // Se il numero utente è maggiore del numero pc l'utente ha vinto, altrimenti vince il pc
+    if(numUtente > numComputer ){
+        console.log("Hai vinto!");
+        alert("Hai vinto!");
+    } else {
+        console.log("Hai perso!");
+        alert("Hai perso!");
+    };
+}
 
